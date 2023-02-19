f151f3a1f937788c673d99bdebbdaea5eab725f3
Damir <damiros.20002@gmail.com>
2023-02-19 11:51:36 +0300

    Добавил классы собственных исключений.

---
+++ src/ExcMissingCurrChar.java
@@ -0,0 +1,5 @@
+public class ExcMissingCurrChar extends Exception {
+    public String ToString(){
+        return("A character is missing from the string.");
+    }
+}
---
+++ src/ExcMoreThanSomeNum.java
@@ -0,0 +1,5 @@
+public class ExcMoreThanSomeNum {
+    public String ToString(){
+        return("More than some number.");
+    }
+}
